51692fb81c4c64b5521150f110871fd09b305673
roni.costa <profronicosta@gmail.com>
2022-07-25 22:33:22 -0300

    Inclusão de Templates

--- Store/views.py
+++ Store/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Store.models import Departamento
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!')
+    meu_nome = 'Beltrano da Costa'
+    sexo = 'M'
+    context = {
+        'nome': meu_nome, 
+        'artigo': 'o' if sexo == 'M' else 'a'
+        }
+    return render(request, 'index.html', context)
 
 def teste(request):
-    return HttpResponse('Minha página de teste')
+    # depto = ['Casa', 'Informática', 'Telefonia', 'Games']
+    depto = Departamento.objects.all()
+    context = {'departamentos': depto}
+    return render(request, 'teste.html', context)
