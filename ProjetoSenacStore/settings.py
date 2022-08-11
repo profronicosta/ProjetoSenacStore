9a8e0b53bac6f1399ee9ac4e1a7d8961c5f9ec74
roni.costa <profronicosta@gmail.com>
2022-08-10 22:39:03 -0300

    remove email e senha

--- ProjetoSenacStore/settings.py
+++ ProjetoSenacStore/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'profronicosta@gmail.com'
-EMAIL_HOST_PASSWORD = 'jfaptkulojoguvck'
+EMAIL_HOST_USER = 'meuemail'
+EMAIL_HOST_PASSWORD = 'minhasenha'
