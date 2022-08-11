3383546bdd5e5b9e834a94867e5ecec407db1350
roni.costa <profronicosta@gmail.com>
2022-08-10 22:20:07 -0300

    Envio de E-mail

--- Store/views.py
+++ Store/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from Store.models import Departamento
 from Store.models import Categoria
 from Store.models import Produto
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
@@ -51,3 +52,20 @@ def institucional(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def enviar_email(request):
+    nome = request.POST['nome']
+    telefone = request.POST['telefone']
+    assunto = request.POST['assunto']
+    mensagem = request.POST['mensagem']
+    remetente = request.POST['email']
+    destinatario = ['profronicosta@gmail.com']
+    corpo = f"Nome: {nome} \nTelefone: {telefone}  \nMensagem: {mensagem}"
+    
+    try:
+        send_mail(assunto, corpo, remetente, destinatario )
+        context = {'msg': 'E-mail enviado com sucesso!'}
+    except:
+        context = {'msg': 'Erro ao enviar e-mail!'}
+   
+    return render(request, 'contato.html', context)
